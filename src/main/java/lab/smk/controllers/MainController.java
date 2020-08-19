870fdfe242dbd901725810bd34e183c9f4c73f95
kyoryoku <kirino3@yandex.by>
2020-08-19 09:33:22 +0300

    1908_0933

--- src/main/java/lab/smk/controllers/MainController.java
+++ src/main/java/lab/smk/controllers/MainController.java
@@ -7,8 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.rmi.CORBA.Util;
-import java.time.LocalDate;
+
 import java.util.Date;
 
 @Controller
