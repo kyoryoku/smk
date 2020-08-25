6d545df8133474fc631ee4d01752692045372501
VittaTanTan <vedodow639@arasj.net>
2020-08-25 17:09:07 +0300

    2508_1708

--- src/main/java/lab/smk/config/WebSecurityConfig.java
+++ src/main/java/lab/smk/config/WebSecurityConfig.java
@@ -27,7 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/" , "/login", "/css/**", "/js/**", "/image/**").permitAll()
+                //отключение авторизации:
+                .antMatchers("/**" , "/login", "/css/**", "/js/**", "/image/**").permitAll()
                 .anyRequest().authenticated()
             .and()
                 .formLogin()
