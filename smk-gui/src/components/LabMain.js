8b54e410b2ab63b578a796521f127db793f0859a
kyoryoku <kirino3@yandex.by>
2020-09-17 17:20:25 +0300

    1709_1720

--- smk-gui/src/components/LabMain.js
+++ smk-gui/src/components/LabMain.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
-
 class LabMain extends Component {
+
+    state = {
+        employees: [],
+        selectedEmployees: []
+    }
+
+    componentDidMount() {
+        const employeesData = [
+            { id: 1, name: 'ООО рога и копыта' },
+            { id: 2, name: 'roy' },
+            { id: 3, name: 'albert' }
+        ]
+        this.setState({
+            employees: employeesData
+        });
+    }
+
+
     render() {
         return (
             <div className='lab-main'>
-                LABMAIN
+
             </div>
         );
     }
--- smk-gui/src/components/navbar/LabSide.js
+++ smk-gui/src/components/navbar/LabSide.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
-import Nav from 'react-bootstrap/Nav';
+
+import { Accordion, AccordionItem } from 'react-sanfona';
 
 class NavSide extends Component {
     render() {
         return (
             <div className='lab-nav-side'>
-                LABSIDE11
+                <Accordion allowMultiple duration={100}>
+                    <AccordionItem title='Журналы 1'>
+                        <div>
+                            <a href="/journal1"> Журнал 1 </a>
+                        </div>
+                        <div>
+                            <a href="/journal2"> Журнал 2 </a>
+                        </div>
+                        <div>
+                            <a href="/journal3"> Журнал 3 </a>
+                        </div>
+                        <div>
+                            <a href="/journal4"> Журнал 4 </a>
+                        </div>
+                        <div>
+                            <a href="/journal5"> Журнал 5 </a>
+                        </div>
+                    </AccordionItem>
+                    <AccordionItem title='Журналы 2'>
+                        <div>
+                            <a href="/journal1"> Журнал 1 </a>
+                        </div>
+                        <div>
+                            <a href="/journal2"> Журнал 2 </a>
+                        </div>
+                        <div>
+                            <a href="/journal3"> Журнал 3 </a>
+                        </div>
+                        <div>
+                            <a href="/journal4"> Журнал 4 </a>
+                        </div>
+                        <div>
+                            <a href="/journal5"> Журнал 5 </a>
+                        </div>
+                    </AccordionItem>
+                    <AccordionItem title='Система'>
+                        <div>
+                            111111111111
+                        </div>
+                    </AccordionItem>
+
+
+
+                </Accordion>
+
             </div>
             
         );
